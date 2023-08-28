d4e10059dccaf3667e3ad35975e6272b8a78e05a
Ismail_Kévin <kevin.ismails3@gmail.com>
2023-08-28 10:42:06 +0200

    Reprise code et ajout de conditions if pour les bullets. Modification aussi du NextSlide qui était présent deux fois et faisait passer les diapos en deux par deux

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -20,6 +20,13 @@ const slides = [
 ];
 
 console.log('images du carroussel chargées');
+const slideImage = document.querySelector('.banner-img');
+const slideText = document.querySelector('.slide-text');
+
+console.log(slideText);
+
+let currentSlide = 0;
+
 
 
 // ETAPE 2 : flèches
@@ -30,13 +37,45 @@ const leftArrow =document.querySelector('.arrow_left');
 rightArrow.addEventListener('click', handleRightClick);
 leftArrow.addEventListener('click', handleLeftClick);
 
-function handleRightClick() {
-	console.log('click droit');
-}
-function handleLeftClick() {
-	console.log('Clic gauche');
-}
+function handleRightClick()  {
+ 
+	
+	nextSlide();
+	console.log(currentSlide)
+	}
 
+	
+	function handleLeftClick() {
+	prevSlide();
+	console.log(currentSlide)
+	}
+	
+	// Ajouter fonction prevSlide
+	
+	function prevSlide() {
+	currentSlide--;
+	
+	if(currentSlide < 0) {
+	currentSlide = slides.length - 1;
+	}
+	
+	displaySlide();
+	changeDot('prev');
+	}
+	// Ajouter fonction nexSlide
+
+	function nextSlide() {
+	
+		currentSlide++;
+		
+		if(currentSlide >= slides.length) {
+		currentSlide = 0;
+		}
+		
+		displaySlide();
+		changeDot('next');
+		}
+		
 // ETAPE 3 : bullets
 
 // Récupérer les points existants
@@ -47,52 +86,73 @@ let numDots = dots.length;
 
 // Définir le point actif
 let currentDot = 0;
+let direction; // déclarer direction pour les dots
 
 // Rendre le premier point actif
 dots[currentDot].classList.add('dot_selected');
 
 // Fonction pour changer le point actif
-function changeDot() {
+function changeDot(dir) {
+	direction= dir;
 
-dots[currentDot].classList.remove('dot_selected');
-
-currentDot++;
-
-if(currentDot >= numDots) {
-currentDot = 0;
-}
-
-dots[currentDot].classList.add('dot_selected');
-
-}
+	// Retirer classe active
+	dots[currentDot].classList.remove('dot_selected');  
+  
+	if(currentSlide === 0 && direction === 'next') {
+	  currentDot = numDots - 1;
+	} else if(currentSlide === slides.length - 1 && direction === 'prev') {
+	  currentDot = 0;
+	} else if(direction === 'next') {
+	  currentDot++; 
+	} else {
+	  currentDot--;
+	}
+  
+	if(currentDot < 0) {
+	  currentDot = numDots - 1;
+	}
+  
+	if(currentDot >= numDots) {
+	  currentDot = 0;
+	}
+  
+	// Ajouter classe active
+	dots[currentDot].classList.add('dot_selected');
+  
+  }
 
 // Gestion des événements
-rightArrow.addEventListener('click', changeDot);
 
 dots.forEach(dot => {
-dot.addEventListener('click', nextSlide)
-});
 
-
+	dot.addEventListener('click', () => {
+  
+	  // Changer slide au clic
+	  currentDot = dots.indexOf(dot);
+	  displaySlide(); 
+	  changeDot();
+  
+	});
+});  
 
 // ETAPE 4 ! 
 
 //selection des éléments
-const slideImage = document.querySelector('banner-img');
-const slidetexte = document.querySelector('slide-text');
 
-let currentSlide = 0;
 
-function nextSlide() {
 	changeDot();
 	const current = slides[currentSlide];
 	slideImage.src = current.image;
 	slideText.innerHTML = current.tagLine;
 
-	currentSlide++;
-	if(currentSlide >= slides.length) {
-		currentSlide = 0
-	}
-}
+	
 
-rightArrow.addEventListener('click', nextSlide);
+
+function displaySlide() {
+	const current = slides[currentSlide];
+	
+	slideImage.src = current.image;
+	slideText.innerHTML = current.tagLine;
+	
+	}
+	
